a9058b2f924b48ee7f0f9a6bf7e1d12602562b3b
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-11-10 10:17:39 -0300

    fix: signin signup

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-//import { authRouter } from "./authRoutes.js";
+import { authRouter } from "./authRoutes.js";
 
 const router = Router();
 
-//router.use(authRouter);
+router.use(authRouter);
 
 export default router;
